705469493752b86c0f86b08518b54aeca2f46039
Nguyen Huu Thuan <thuannh@openwaygroup.com>
2019-11-01 17:49:29 +0700

    [INTERNAL] Init the project

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+const helmet = require('helmet');
+
+const PORT = process.env.PORT || 3000;
+var app = express();
+app.use(helmet());
+
+var router = express.Router();
+console.log("running");
+router.use('/', (res, req, next) => {
+    console.log('Request URL:', req.originalUrl)
+    next()
+}, function (req, res, next) {
+    console.log('Request Type:', req.method)
+    next()
+})
+
+app.listen(PORT, () => {
+
+    console.log(`Listening on http://localhost:${PORT}`);
+});
